984464a3836575afd8bb97a53cef9c6ed505fd83
han45678 <han0910812144@gmail.com>
2026-01-20 08:28:53 +0800

    s3

--- src/section/s3.vue
+++ src/section/s3.vue
@@ -1,8 +1,21 @@
 <template>
-  <article class="s3" id="s3">
+  <article
+    class="s3"
+    id="s3"
+  >
     <div class="text">
-      <img src="./s3/t1.svg" alt="title">
-      <p>台南門戶計畫因半導體及世界AI巨擘擴大投資台南而生，台南車站4站距離可抵南科與高鐵，透過交通轉運與科技發展緊密串連。</p>
+      <img
+        class="pic"
+        src="./s3/pic.jpg"
+        alt="pic"
+      />
+      <img
+        src="./s3/t1.svg"
+        alt="title"
+      />
+      <p>
+        台南門戶計畫因半導體及世界AI巨擘擴大投資台南而生，台南車站4站距離可抵南科與高鐵，透過交通轉運與科技發展緊密串連。
+      </p>
       <p>
         台南門戶計畫主要結合了鐵路地下化、車站TOD再造計畫、三座商業大樓(8萬坪招商面積)、綠園道、並增設2站地下化車站(林森站、南台南)，消除地面鐵路對都市發展的阻礙，並配合都市更新，打造現代化的交通樞紐，國際級CBD。
       </p>
@@ -10,8 +23,12 @@
         「成大之森」緊鄰台南門戶計畫核心，絕對是您置產投資致富的關鍵標的！
       </p>
     </div>
-    
-    <img src="./s3/bg.jpg" alt="bg" class="bg-img">
+
+    <img
+      src="./s3/bg.jpg"
+      alt="bg"
+      class="bg-img"
+    />
 
     <div class="caption">
       台南車站3D情境示意圖
@@ -26,9 +43,9 @@
 .s3 {
   position: relative;
   width: 100%;
-  height: 100vh; // 設定高度為視窗高度
+  // height: 100vh; // 設定高度為視窗高度
   overflow: hidden;
-  font-family: "Noto Sans TC", sans-serif; // 建議使用無襯線字體
+  font-family: 'Noto Sans TC', sans-serif; // 建議使用無襯線字體
 
   // 1. 背景圖層設定 (絕對定位，置底)
   .bg-img {
@@ -51,18 +68,21 @@
     align-items: center;
     width: 100%;
     height: 100%; // 佔滿高度以應用漸層
-    padding-top: 15vh; // 標題距離頂部的空間
-    
+    padding-top: size(184); // 標題距離頂部的空間
+    padding-bottom: size(780);
+
     // **關鍵修改：加入白色半透明漸層背景**
     background: linear-gradient(
       to bottom,
-      rgba(255, 255, 255, 1) 0%,    // 頂部純白
-      rgba(255, 255, 255, 0.8) 50%, // 中間半透明
-      rgba(255, 255, 255, 0) 100%   // 底部透明
+      rgba(255, 255, 255, 1) 0%,
+      // 頂部純白
+      rgba(255, 255, 255, 0.8) 50%,
+      // 中間半透明
+      rgba(255, 255, 255, 0) 100% // 底部透明
     );
 
     // 標題 SVG
-    > img[alt="title"] {
+    > img[alt='title'] {
       width: auto;
       height: 60px; // 依實際 SVG 調整
       margin-bottom: 40px;
@@ -96,7 +116,7 @@
           background-color: #a0a0a0; // 淺灰色線條
         }
       }
-      
+
       // 最後一段強調文字
       &:last-child {
         font-weight: 500;
@@ -117,7 +137,7 @@
     font-size: 14px;
     letter-spacing: 1.5px;
     font-weight: 500;
-    text-shadow: 1px 1px 3px rgba(0,0,0,0.5); // 增加陰影提高可讀性
+    text-shadow: 1px 1px 3px rgba(0, 0, 0, 0.5); // 增加陰影提高可讀性
 
     span {
       display: block;
@@ -134,14 +154,18 @@
   .s3 {
     height: auto; // 手機版高度自動
     min-height: 100vh;
-    
+
     .text {
       padding-top: 100px;
       padding-bottom: 50px;
       // 手機版漸層可以調整或移除
-      background: linear-gradient(to bottom, rgba(255,255,255,1) 0%, rgba(255,255,255,0.5) 100%);
+      background: linear-gradient(
+        to bottom,
+        rgba(255, 255, 255, 1) 0%,
+        rgba(255, 255, 255, 0.5) 100%
+      );
 
-      > img[alt="title"] {
+      > img[alt='title'] {
         height: 40px;
         margin-bottom: 30px;
       }
@@ -149,17 +173,17 @@
         font-size: 14px;
         line-height: 1.8;
         max-width: 100%;
-        
+
         &:first-of-type {
           margin-top: 30px;
           &::before {
-             top: -30px;
-             height: 15px;
+            top: -30px;
+            height: 15px;
           }
         }
       }
     }
-    
+
     .caption {
       position: relative; // 手機版改為相對定位，避免擋住內容
       bottom: auto;
